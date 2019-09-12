9e3d718bd9c2a54fdbee72152b5d99845888844a
malinda666 <malinda666@users.noreply.github.com>
2019-09-12 20:45:19 +0530

    ediot 1

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ const isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
 const isChrome = navigator.userAgent.indexOf("Chrome") > -1;
 
 const colors = {
-    white: '0xffffff',
-    black: '0x000000',
+    amber: '0xcebc81',
+    black: '0x2a363b',
+    orange : '0xff847c',
     trans : 'transparent',
-    darkWhite : '0xfefefe'
 }
 function lerp(start, end, amt) {
     return (1 - amt) * start + amt * end;
@@ -46,13 +46,14 @@ const getMousePos = (ev) => {
 
 const main = document.querySelector('.main');
 const content = document.querySelector('.scroll-content');
+anime.set(main, { overflow: 'hidden'});
+anime.set(content, { overflow: 'hidden',paddingTop : window.innerHeight * 3 });
+anime.set('.work', { paddingBottom : window.innerHeight});
 
 const limit = main.getBoundingClientRect().height - window.innerHeight;
 function initHome() {
     document.body.classList.add('loading');
-    anime.set(main, { overflow: 'hidden'});
-    anime.set(content, { overflow: 'hidden' });
-    
+   
     console.log('init-home');
     setTimeout(() => {
         document.body.classList.remove('loading');
@@ -81,110 +82,187 @@ function initEverything(){
     const app = new PIXI.Application(window.innerWidth,window.innerHeight,{
         transparent : true,
         resolution : 2,
-        antialias : false,
+        antialias : true,
         autoResize: true
     });
     app.view.setAttribute('id','canvas');
     main.appendChild(app.view);
 
+    var mainBg = new PIXI.Graphics;
+    mainBg.beginFill(colors.amber);
+    mainBg.drawRect(0,0,window.innerWidth,window.innerHeight);
+    app.stage.addChild(mainBg);
+    
     var cont = new PIXI.Container();
     app.stage.addChild(cont);
-
-    var scrolltxt = new PIXI.Text('scroll',{
+       
+    var introtxt_1 = new PIXI.Text('Simple_M.',{
         fontFamily : 'league',
-        fontSize : 50,
-        fontWeight : 700,
+        fontSize : 120,
+        fontWeight : 900,
         fill : colors.black
-    })
-    scrolltxt.anchor.set(0.5,0.5);
-    scrolltxt.x = window.innerWidth/2;
-    scrolltxt.y = window.innerHeight/2;
-    cont.addChild(scrolltxt);
+    });
+    introtxt_1.anchor.set(0.5,0.5);
+    introtxt_1.x = window.innerWidth/2;
+    introtxt_1.y = window.innerHeight/2;
+    cont.addChild(introtxt_1);
+
+    var mask_one = new PIXI.Graphics;
+    mask_one.beginFill(0);
+    mask_one.drawRect(0,0,window.innerWidth,window.innerHeight);
+    app.stage.addChild(mask_one); 
 
     var bgOne = new PIXI.Graphics;
     bgOne.beginFill(colors.black);
     bgOne.drawRect(0,0,window.innerWidth,window.innerHeight);
-    cont.addChild(bgOne);
+    bgOne.mask = mask_one;
+    app.stage.addChild(bgOne); 
 
-    // var mask_1 = new PIXI.Graphics;
-    // mask_1.beginFill(colors.black);
-    // mask_1.drawRect(0,0,window.innerWidth,window.innerHeight);
-    // cont.addChild(mask_1);
+    var cont_2 = new PIXI.Container();
+    app.stage.addChild(cont_2);
 
-    var txt_1 = new PIXI.Text('Creative',{
+    var introtxt_2 = new PIXI.Text('Simple_M.',{
         fontFamily : 'league',
-        fontSize : 200,
+        fontSize : 120,
         fontWeight : 900,
-        fill : colors.darkWhite,
-        // stroke : colors.black,
-        // strokeThickness : 1.5
-    })
-    txt_1.anchor.set(0.5,0.5);
-    txt_1.x = 0;
-    txt_1.y = window.innerHeight/3;
-    cont.addChild(txt_1);
+        fill : colors.amber
+    });
+    introtxt_2.anchor.set(0.5,0.5);
+    introtxt_2.x = window.innerWidth/2;
+    introtxt_2.y = window.innerHeight/2;
+    cont_2.addChild(introtxt_2);
+    cont_2.mask = mask_one;
 
-    var txt_2 = new PIXI.Text('Developer',{
+    var scrolltxt = new PIXI.Text('scroll to discover',{
         fontFamily : 'league',
-        fontSize : 200,
-        fontWeight : 900,
-        fill : colors.darkWhite,
-        // stroke : colors.black,
-        // strokeThickness : 1.5
-    })
-    txt_2.anchor.set(0.5,0.5);
-    txt_2.x = 0;
-    txt_2.y = window.innerHeight/1.5;
-    cont.addChild(txt_2);
+        fontSize : 15,
+        fontWeight : 600,
+        fill : colors.black
+    });
+    scrolltxt.anchor.set(0.5,0.5);
+    scrolltxt.x = window.innerWidth/2;
+    scrolltxt.y = window.innerHeight -scrolltxt.height;
+    app.stage.addChild(scrolltxt);
 
-    
     var bgTwo = new PIXI.Graphics;
-    bgTwo.beginFill(colors.darkWhite);
-    bgTwo.drawRect(0,0,window.innerWidth,window.innerHeight*8);
-    cont.addChild(bgTwo);
+    bgTwo.beginFill(colors.amber);
+    bgTwo.drawRect(0,0,window.innerWidth,window.innerHeight);
+    app.stage.addChild(bgTwo);
+    
+    
+    var introdesc_1 = new PIXI.Text("Hi..I\'m Malinda Anupama.",{
+        fontFamily : 'league',
+        fontSize : 45,
+        fontWeight : 900,
+        fill : colors.amber
+    });
+    introdesc_1.anchor.set(0.5,0.5);
+    introdesc_1.alpha = 0;
+    introdesc_1.x = window.innerWidth/2 - introdesc_1.width/4;
+    introdesc_1.y = window.innerHeight - introdesc_1.height*2;
+    app.stage.addChild(introdesc_1);
+    
+    var introdesc_2 = new PIXI.Text("Freelance Web Designer.",{
+        fontFamily : 'league',
+        fontSize : 45,
+        fontWeight : 900,
+        fill : colors.amber
+    });
+    introdesc_2.anchor.set(0.5,0.5);
+    introdesc_2.alpha = 0;
+    introdesc_2.x = window.innerWidth/2 + introdesc_2.width/4;
+    introdesc_2.y = window.innerHeight - introdesc_2.height;
+    app.stage.addChild(introdesc_2);
 
+    var fancytxt_1 = new PIXI.Text('Creative',{
+        fontFamily : 'monument',
+        fontSize : 300,
+        fontWeight : 900,
+        fill : colors.black
+    });
+    fancytxt_1.anchor.set(0.5,0.5);
+    fancytxt_1.x = -window.innerWidth;
+    fancytxt_1.y = window.innerHeight/4;
+    app.stage.addChild(fancytxt_1);
+    
+    var fancytxt_2 = new PIXI.Text('Developer',{
+        fontFamily : 'monument',
+        fontSize : 300,
+        fontWeight : 900,
+        fill : colors.black
+    });
+    fancytxt_2.anchor.set(0.5,0.5);
+    fancytxt_2.x = window.innerWidth + fancytxt_2.width;
+    fancytxt_2.y = window.innerHeight/2 + fancytxt_2.height/3;
+    app.stage.addChild(fancytxt_2);
+
+    var bgThree = new PIXI.Graphics;
+    bgThree.beginFill(colors.orange);
+    bgThree.drawRect(0,0,window.innerWidth,window.innerHeight);
+    app.stage.addChild(bgThree);
 
     var tl = anime.timeline({
         loop: false,
-        autoplay: false,
+        //autoplay: false,
         delay : 0,
     });
-    tl.add({
-        targets : bgOne,
-        x : [window.innerWidth,0],
-        easing : 'easeOutQuart',
-        duration:1000
-    })
-    tl.add({
-        targets : scrolltxt,
-        alpha : [1,0],
-        easing : 'easeOutQuart',
-        duration:1000
-    })
-    tl.add({
-        targets : txt_1,
-        x : [-window.innerWidth*1.5,window.innerWidth/2],
-        easing : 'easeOutQuart',
-        duration:2000
-    },2000)
-    tl.add({
-        targets : txt_2,
-        x : [window.innerWidth*1.5,window.innerWidth/2],
-        easing : 'easeOutQuart',
-        duration:2000
-    },2000)
-    tl.add({
-        targets : bgOne,
-        x : [0,-window.innerWidth],
-        easing : 'easeOutQuad',
-        duration:3000
-    })
-    tl.add({
+    tl
+    .add({
+        targets : mask_one,
+        y : [window.innerHeight,0],
+        duration : 1000,
+        easing : 'easeOutExpo'
+    },0)
+    .add({
+        targets : introtxt_1,
+        y : [window.innerHeight/2,window.innerHeight/2 - 300],
+        duration : 4500,
+        easing : 'easeOutExpo'
+    },0)
+    .add({
+        targets : introtxt_2,
+        y : [window.innerHeight/2,window.innerHeight/2 - 300],
+        duration : 4500,
+        easing : 'easeOutExpo'
+    },0)
+    .add({
+        targets : introdesc_1,
+        x : [window.innerWidth/2 - introdesc_1.width/4,window.innerWidth/2],
+        alpha : 1,
+        duration : 2800,
+        easing : 'easeOutExpo'
+    },100)
+    .add({
+        targets : introdesc_2,
+        x : [window.innerWidth/2 + introdesc_2.width/4,window.innerWidth/2],
+        alpha : 1,
+        duration : 2800,
+        easing : 'easeOutExpo'
+    },100)
+    .add({
         targets : bgTwo,
-        y : [window.innerHeight*1.5,-window.innerHeight],
-        easing : 'easeOutQuad',
-        duration:3000
-    })
+        y : [window.innerHeight,0],
+        duration : 1000,
+        easing : 'easeOutExpo'
+    },1000)
+    .add({
+        targets : fancytxt_1,
+        x : [-window.innerWidth,window.innerWidth + fancytxt_1.width],
+        duration : 2500,
+        easing : 'easeOutSine'
+    },1200)
+    .add({
+        targets : fancytxt_2,
+        x : [window.innerWidth + fancytxt_2.width,-window.innerWidth],
+        duration : 2500,
+        easing : 'easeOutSine'
+    },1200)
+    .add({
+        targets : bgThree,
+        y : [window.innerHeight,0],
+        duration : 1000,
+        easing : 'easeOutQuart'
+    },3000)
     
     
     
@@ -207,8 +285,8 @@ function initEverything(){
         requestAnimationFrame(update);
         let p = 0;
         currentY += (targetY - currentY) * ease;
-        transform(content, 0, currentY, 0.2)
-       tl.seek((currentY / -1000) * tl.duration);
+        transform(content, 0, currentY)
+        tl.seek((currentY / -2000) * tl.duration);
     }
 
     function transform(element, x, y, delay) {
